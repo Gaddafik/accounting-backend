8b7ad23cd6d3c1ff98527a37cf44afea55e449ff
Gaddafik <113804800+Gaddafik@users.noreply.github.com>
2025-11-17 03:02:01 +0100

    Update and rename index.js to server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5000;
 // ========= MIDDLEWARE =========
 app.use(bodyParser.json());
 app.use(cors({
-  origin: 'https://portalsample.netlify.app', // Replace with your Netlify URL
+  origin: 'https://portalsample.netlify.app', 
   credentials: true
 }));
 
